be7f3c1761992fe3f08a250165f5a9a4a0a9edf8
skyiekoltepatil <bhushankolte20@gmail.com>
2025-10-26 21:57:46 +0530

    section 3 lecture 36
    Function Expression
    Arrow Function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -40,36 +40,8 @@ const age3 = calcAge3(2010);
 console.log(age3);  
 const yearsUntilRetirement = (birthYear, firstName) => {
   const age = 2037 - birthYear;
-  const retirement = 65 - age;  
-    return `${firstName} retires in ${retirement} years.`;      
+  const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`;
 }
 console.log(yearsUntilRetirement(2008, 'Skyie'));
 console.log(yearsUntilRetirement(2007, 'Shruti'));
-// Functions Calling Other Functions
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);  
-  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
-console.log(fruitProcessor(2, 3));
-// Reviewing Functions
-const calcAge = function (birthYear) {  
-    return 2037 - birthYear;
-}
-const yearsUntilRetirement = function (birthYear, firstName) {
-  const age = calcAge(birthYear);
-  const retirement = 65 - age;  
-    if (retirement > 0) {    
-        console.log(`${firstName} retires in ${retirement} years.`);      
-        return retirement;
-    } else {
-        console.log(`${firstName} has already retired.`);      
-        return -1;
-    }
-}
-console.log(yearsUntilRetirement(2008, 'Skyie'));
-console.log(yearsUntilRetirement(1950, 'Mike'));
