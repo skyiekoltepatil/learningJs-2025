57036a26b7fac283c94d5b8bd46e95365376d441
skyiekoltepatil <bhushankolte20@gmail.com>
2025-10-26 21:09:33 +0530

    basic of JavaScript
    contains basic of JavaScript :
    1. template literals
    2. Boolean logic
    3. Ternary operators
    4. Data types and variable definition
    5.strick equality vs normal equality operator
    6. type coercion and type conversion
    4.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,113 @@
+
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+// Values and Variables
+console.log("skyie@");
+console.log(18);
+
+let userFirstName = "skyie@";
+
+console.log(userFirstName);
+console.log(userFirstName);
+console.log(userFirstName);
+console.log(userFirstName);
+// Variable name conventions
+let skyieAt = "skyie";
+let $function = 18;
+
+let person = "skyie@";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+////////////////////////////////////
+// let, const and var
+let age = 18;
+age = 18;
+
+const birthYear = 2007;
+// birthYear = 2007;
+// const job;
+
+job1 = 'programmer';
+job2 = 'teacher'
+
+let userName = 'skyie';
+letLastName = 'koltepatil';
+console.log(userName + ' ' + letLastName);
+
+// Basic Operators
+// Math operators
+const now = 2037;
+const ageskyie = now - 2008;
+const ageshruti = now - 2007;
+console.log(ageskyie,ageshruti);
+
+console.log(ageskyie * 2, ageskyie/ 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'skyie';
+const lastName = 'koltepatil';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageskyie > ageshruti); // >, <, >=, <=
+console.log(ageshruti >= 18);
+
+const isFullAge = ageshruti>= 18;
+
+console.log(now - 2008 > now - 2007);
+console.log(25 - 10 - 5);
+
+// Operator Precedence
+const now1 = 2037;
+const ageSkyie = now1 - 2008;
+const ageShruti = now1 - 2007;  
+console.log(now1 - 2008 > now1 - 2007);
+
+let x1, y1;
+x1 = y1 = 25 - 10 - 5; // x1 = y1 = 10
+console.log(x1, y1);        
+const averageAge = (ageSkyie + ageShruti) / 2
+console.log(ageSkyie, ageShruti, averageAge);
+
+
+<script src="script.js" defer></script>
